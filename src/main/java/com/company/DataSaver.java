12c34986c75adcf49e4d675f1ab7ed6d9400668b
crowabout <crowabout@yeah.net>
2017-09-02 13:20:24 +0800

    complete download function

---
+++ src/main/java/com/company/DataSaver.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by pc on 2017/9/2.
+ */
+public class DataSaver {
+}
---
+++ src/main/java/com/company/net/HttpDowerTask.java
@@ -0,0 +1,7 @@
+package com.company.net;
+
+/**
+ * Created by pc on 2017/9/2.
+ */
+public class HttpDowerTask {
+}
